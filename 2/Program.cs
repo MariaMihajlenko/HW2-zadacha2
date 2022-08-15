7799f3e627d24f29c41f59b56c30a6a6240b13b3
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-15 11:26:42 +0500

    zadacha2

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class LengthDemo
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Введите любое число:  ");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        char[] arr = numbers.ToString().ToCharArray();
+        Console.WriteLine("Размерность массива:  ");
+        Console.WriteLine(arr.Length);
+        if (arr.Length > 2 && arr.Length < 4)
+        {
+            Console.WriteLine("Третье число массива:  ");
+            var n = arr[2];
+            Console.WriteLine(n);
+        }
+
+
+        Console.WriteLine("Число не имеет третьего элемента.");
+
+
+    }
+
+}
